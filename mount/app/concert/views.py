c50c9fb5d17d528a16ce3453a056a5c2eeeeba15
Harmanpreet Singh <harmanpreet181001@gmail.com>
2025-04-13 09:15:28 +0530

    MODIFY: change url name

--- mount/app/concert/views.py
+++ mount/app/concert/views.py
@@ -14,6 +14,6 @@ async def create_concert(data: ConcertCreate, db: AsyncSessionDep):
     return await create_item_from_json(db, Concert, data.model_dump(exclude_unset=True))
 
 
-@concert_router.patch("/venue/{venue_id}/concerts/{concert_id}/add")
+@concert_router.patch("/concerts/{concert_id}/set-venue/{venue_id}")
 async def add_concert_to_venue(db: AsyncSessionDep, venue_id: int, concert_id: int):
     return await add_concert_to_venue_db(db, concert_id, venue_id)
